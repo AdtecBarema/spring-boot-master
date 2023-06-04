b647345bee9d6517b68d2a061ccf53f6cafef9ba
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-06-04 16:28:14 -0500

    spring initializer added

---
+++ springbootmaster/src/test/java/com/addtech/springbootmaster/SpringbootmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addtech.springbootmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
