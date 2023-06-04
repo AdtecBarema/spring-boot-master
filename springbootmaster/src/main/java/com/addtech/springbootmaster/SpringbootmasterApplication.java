70b413621b820a1d0f98b9bcd9d0c4694ff6ae60
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-06-04 16:44:43 -0500

    section-1-getting started

--- springbootmaster/src/main/java/com/addtech/springbootmaster/SpringbootmasterApplication.java
+++ springbootmaster/src/main/java/com/addtech/springbootmaster/SpringbootmasterApplication.java
@@ -2,12 +2,47 @@ package com.addtech.springbootmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootmasterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootmasterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmasterApplication.class, args);
+    }
+
+    @GetMapping("/")
+    Customer getCustomer() {
+        return new Customer(100L, "Addisu");
+    }
+
+    class Customer {
+
+        private final Long id;
+        private final String name;
+
+        Customer(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
 
 }
