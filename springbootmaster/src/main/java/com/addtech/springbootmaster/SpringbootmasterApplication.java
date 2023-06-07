7df4ab8088dcc1de622c3e409c1c7fafbd0f94c2
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-06-06 22:40:40 -0500

    section-10-open-feign

--- springbootmaster/src/main/java/com/addtech/springbootmaster/SpringbootmasterApplication.java
+++ springbootmaster/src/main/java/com/addtech/springbootmaster/SpringbootmasterApplication.java
@@ -2,11 +2,10 @@ package com.addtech.springbootmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@EnableOpenfeignClients
 public class SpringbootmasterApplication {
 
     public static void main(String[] args) {
